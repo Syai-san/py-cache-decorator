504cd5b943fa5ce684ba940e804cdefde762a836
Syai <syai.sichkar@gmail.com>
2025-05-02 13:39:07 +0200

    Decorator @cache commit 1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-from typing import Callable
+from functools import wraps
+from typing import Callable, Any
 
 
-def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+def cache(func: Callable) -> Callable :
+        cached_results = {}
+
+        @wraps(func)
+        def wrapper(*args: Any) -> Any:
+            cache_key = (func, args)
+            if cache_key in cached_results:
+                print("Getting from cache")
+                return cached_results[cache_key]
+
+            print("Calculating new result")
+            result = func(*args)
+            cached_results[cache_key] = result
+            return result
+
+        return wrapper
+
+
+

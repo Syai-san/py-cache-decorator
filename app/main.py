86c44f5af46577ec8bfdc1f83263085b4a7bb6e5
Syai <syai.sichkar@gmail.com>
2025-05-02 13:47:49 +0200

    Decorator @cache commit 2

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 from functools import wraps
-from typing import Callable, Any
+from typing import Callable, Any, Dict, Tuple
 
 
 def cache(func: Callable) -> Callable :
-        cached_results = {}
+    cached_results: Dict[Tuple[Any, ...], Any] = {}
 
-        @wraps(func)
-        def wrapper(*args: Any) -> Any:
-            cache_key = (func, args)
-            if cache_key in cached_results:
-                print("Getting from cache")
-                return cached_results[cache_key]
+    @wraps(func)
+    def wrapper(*args: Any) -> Any:
+        if args in cached_results:
+            print("Getting from cache")
+            return cached_results[args]
 
-            print("Calculating new result")
-            result = func(*args)
-            cached_results[cache_key] = result
-            return result
+        print("Calculating new result")
+        result = func(*args)
+        cached_results[args] = result
+        return result
 
-        return wrapper
-
+    return wrapper
 
 
